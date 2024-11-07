27f09a573f8549911e1fee60fc4784b0df5ee955
jonson <15096429+jonson1@user.noreply.gitee.com>
2024-11-07 09:17:24 +0800

    Failure test code

--- function/getFunction.py
+++ function/getFunction.py
@@ -38,7 +38,7 @@ def get_proxy():
 def delete_proxy(proxy):
     requests.get("http://127.0.0.1:5010/delete/?proxy={}".format(proxy))
 # your spider code
-def getHtml(url,retry_count = 5):
+def get(url,retry_count = 5):
     proxy = get_proxy().get("proxy")
     while retry_count > 0:
         try:
@@ -52,7 +52,7 @@ def getHtml(url,retry_count = 5):
     # 删除代理池中代理
     delete_proxy(proxy)
     return None
-def get(info_url,retry_count = 5):
+def getDome(info_url,retry_count = 5):
     while retry_count > 0:
         try:
             req = requests.get(info_url, headers=get_random_ua())
